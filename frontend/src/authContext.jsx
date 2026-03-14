1fc003e688fa3c1358c236bd70e1d0d3399ce825
MANIKRISHNA <marumallamanikrishna123@gmail.com>
2026-03-14 18:54:40 +0530

    removing auth

--- frontend/src/authContext.jsx
+++ frontend/src/authContext.jsx
@@ -1,21 +1,21 @@
 // AuthContext.js
-import { createContext, useState, useEffect } from "react";
+// import { createContext, useState, useEffect } from "react";
 
-export const AuthContext = createContext();
+// export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+// export const AuthProvider = ({ children }) => {
+//   const [token, setToken] = useState(null);
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem("token");
-    if (storedToken) {
-      setToken(storedToken);
-    }
-  }, []);
+//   useEffect(() => {
+//     const storedToken = localStorage.getItem("token");
+//     if (storedToken) {
+//       setToken(storedToken);
+//     }
+//   }, []);
 
-  return (
-    <AuthContext.Provider value={{ token, setToken }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider value={{ token, setToken }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './authContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
       <BrowserRouter>
           <App />
       </BrowserRouter>
-  </AuthProvider>
 )
